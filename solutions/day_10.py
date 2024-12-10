a4283d7ae6029c606928c75b90779987ffa704ef
Gasper Martic <martic.gasper@gmail.com>
2024-12-10 16:01:54 +0100

    Optimize day_10 further by combining part_0 and part_1 into part_0_and_1
    Signed-off-by: Gasper Martic <martic.gasper@gmail.com>

--- solutions/day_10.py
+++ solutions/day_10.py
@@ -2,38 +2,6 @@ import time
 from utils import get_input
 
 
-def get_visited_unique(map, i, j, prev_height, visited):
-    map_len = len(map)
-    # check if we have gone out of bounds
-    if i == map_len or j == map_len or i < 0 or j < 0:
-        return
-    curr_height = map[i][j]
-    # check if we climbed exactly one step
-    if curr_height != prev_height + 1:
-        return
-    # add location to set of visited locations if we have reached the top
-    if curr_height == 9:
-        visited.add((i, j))
-        return
-    # else walk in all directions
-    get_visited_unique(map, i + 1, j, curr_height, visited)
-    get_visited_unique(map, i - 1, j, curr_height, visited)
-    get_visited_unique(map, i, j + 1, curr_height, visited)
-    get_visited_unique(map, i, j - 1, curr_height, visited)
-
-
-def part_0(map):
-    map_len = len(map)
-    count = 0
-    for i in range(map_len):
-        for j in range(map_len):
-            if map[i][j] == 0:
-                visited_unique = set()
-                get_visited_unique(map, i, j, -1, visited_unique)
-                count += len(visited_unique)
-    return count
-
-
 def get_visited(map, i, j, prev_height, visited):
     map_len = len(map)
     # check if we have gone out of bounds
@@ -54,16 +22,18 @@ def get_visited(map, i, j, prev_height, visited):
     get_visited(map, i, j - 1, curr_height, visited)
 
 
-def part_1(map):
+def part_0_and_1(map):
     map_len = len(map)
-    count = 0
+    count_0 = 0
+    count_1 = 0
     for i in range(map_len):
         for j in range(map_len):
             if map[i][j] == 0:
                 visited = []
                 get_visited(map, i, j, -1, visited)
-                count += len(visited)
-    return count
+                count_0 += len(set(visited))
+                count_1 += len(visited)
+    print(f"{count_0}\n{count_1}")
 
 
 if __name__ == "__main__":
@@ -71,6 +41,5 @@ if __name__ == "__main__":
     map = []
     for line in get_input():
         map.append([int(el) for el in line])
-    print(part_0(map))
-    print(part_1(map))
+    part_0_and_1(map)
     print(f"Execution time: {time.time() - start_time}s")
